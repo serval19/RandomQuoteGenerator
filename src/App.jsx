ee2c2b973835a35cc72b96f1958336162d65ddda
Diwa Tomy <diwatomy04@gmail.com>
2025-02-06 00:17:22 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Randomquote from './Randomquote.jsx'
+function App() {
+  
+
+  return (
+    
+      <Randomquote />
+  
+  );
+}
+
+export default App
---
+++ src/Randomquote.jsx
@@ -0,0 +1,13 @@
+
+function randomquotegen(){
+    return(
+        <div className="container">
+            <h1>Random Quote Generator</h1>
+            <button className="button">Generate a random quote</button>
+           
+
+        </div>
+
+    )
+}
+export default randomquotegen
