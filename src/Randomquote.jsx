aec6c078c4c8fada438b6e5f7cdca3264a305a5a
Diwa Tomy <diwatomy04@gmail.com>
2025-02-06 23:24:32 +0530

    button changes

--- src/Randomquote.jsx
+++ src/Randomquote.jsx
@@ -1,13 +1,16 @@
 
 function randomquotegen(){
     return(
+        <>
+        <div className="top"></div>
         <div className="container">
             <h1>Random Quote Generator</h1>
             <button className="button">Generate a random quote</button>
+            <p className="quote"></p>
            
 
         </div>
-
+        </>
     )
 }
 export default randomquotegen
