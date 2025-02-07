228e054a92ed7e84ae141eae1d03045a6c9390f6
Diwa Tomy <diwatomy04@gmail.com>
2025-02-07 22:16:15 +0530

    added fucntion to generate fact and usestate variable

--- src/Randomquote.jsx
+++ src/Randomquote.jsx
@@ -1,12 +1,23 @@
-
+import { useState } from "react"
 function randomquotegen(){
+    const [fact,setFact]=useState('');
+    document.title="Random Fact Generator"
+    const fetchrandomfact=async ()=>{
+        const url="https://uselessfacts.jsph.pl/random.json?language=en"
+        const response=await fetch(url)
+        console.log(response);
+        
+    }
     return(
         <>
         <div className="top"></div>
         <div className="container">
             <h1>Random Quote Generator</h1>
-            <button className="button">Generate a random quote</button>
+            <button className="button" onClick={fetchrandomfact}>Generate a random quote</button>
+            <br/>
+            <br/>
             <p className="quote"></p>
+            
            
 
         </div>
